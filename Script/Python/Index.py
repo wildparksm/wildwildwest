e4f1cfd36d755bd0d5ed7523f10f1d4b2107c03e
wildparksm <parksm80@gmail.com>
2023-06-07 08:55:27 +0900

    202306070855

--- Script/Python/Index.py
+++ Script/Python/Index.py
@@ -1,34 +1,24 @@
-import requests
+import investpy
+import yfinance as yf
 import matplotlib.pyplot as plt
 
-# 환율 API URL
-url_exchange = "https://api.investing.com/api/v1/get_chart_data.json?symbol=EURUSD&interval=15&from_date=2023-05-29&to_date=2023-06-01"
+# 미국 달러 인덱스 데이터 가져오기
+try:
+    us_dollar_index = yf.Ticker("DXY").history(start="2022-06-01", end="2023-05-30")
+except Exception as e:
+    print(f"Error while fetching data for ^DXY: {e}")
 
-# 코스피 API URL
-url_kosdaq = "https://api.investing.com/api/v1/get_chart_data.json?symbol=KS11&interval=15&from_date=2023-05-29&to_date=2023-06-01"
+# 대한민국의 코스피 데이터 가져오기
+try:
+    kosdaq = yf.Ticker("KS11").history(start="2022-06-01", end="2023-05-30")
+except Exception as e:
+    print(f"Error while fetching data for KS11: {e}")
 
-# 환율 API 요청
-response_exchange = requests.get(url_exchange)
-
-# 코스피 API 요청
-response_kosdaq = requests.get(url_kosdaq)
-
-# 환율 데이터 추출
-exchange_data = response_exchange.json()["Data"]
-
-# 코스피 데이터 추출
-kosdaq_data = response_kosdaq.json()["Data"]
-
-# 환율 데이터 시각화
-plt.plot(exchange_data["Date"], exchange_data["Close"])
-plt.title("EURUSD Exchange Rate")
-plt.xlabel("Date")
-plt.ylabel("Price")
-plt.show()
-
-# 코스피 데이터 시각화
-plt.plot(kosdaq_data["Date"], kosdaq_data["Close"])
-plt.title("KOSPI Index")
-plt.xlabel("Date")
-plt.ylabel("Price")
-plt.show()
+# 두 데이터를 겹쳐서 그리기
+if 'us_dollar_index' in locals() and 'kosdaq' in locals():
+    plt.plot(us_dollar_index["Close"], label="US Dollar Index", color="pink", linewidth=2)
+    plt.plot(kosdaq["Close"], label="KOSPI", color="green", linewidth=2)
+    plt.legend()
+    plt.show()
+else:
+    print("Could not plot chart due to missing data.")
