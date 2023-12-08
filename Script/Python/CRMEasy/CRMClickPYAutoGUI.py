cf30479b41dd9b008232d2cc018e55f69995e926
박상민 <parksm80@gmail.com>
2023-12-08 11:18:56 +0900

    202312081118

---
+++ Script/Python/CRMEasy/CRMClickPYAutoGUI.py
@@ -0,0 +1,44 @@
+import pyautogui
+import webbrowser
+import time
+import pyperclip
+
+# 웹 브라우저를 열고 특정 URL로 이동
+url = 'https://tdg.crm5.dynamics.com/main.aspx?appid=b484f1b8-fa18-eb11-a813-000d3a854084&forceUCI=1&pagetype=entityrecord&etn=serviceappointment'
+webbrowser.open(url)
+# 웹 페이지가 로드되는 동안 잠시 대기
+time.sleep(5)
+
+# 서비스 케이스
+pyautogui.moveTo(2958, 393)
+pyautogui.click()
+pyautogui.moveTo(4658, 420)
+pyautogui.click()
+pyperclip.copy('[삼성전기] 인도법인 VDI 내부지원')
+pyautogui.hotkey('ctrl', 'v')
+time.sleep(2)
+# pyautogui.press('enter')
+pyautogui.moveTo(3078, 469)
+pyautogui.click(button='left')
+# 제목
+pyautogui.moveTo(2960, 450)
+pyautogui.click(button='left')
+pyperclip.copy('[삼성전기] 인도법인 VDI 운영 지원')
+pyautogui.hotkey('ctrl', 'v')
+time.sleep(2)
+
+pyautogui.moveTo(3975, 583)
+pyautogui.click(button='left')
+pyperclip.copy('480')
+pyautogui.hotkey('ctrl', 'v')
+
+pyautogui.moveTo(2948, 773)
+pyautogui.click(button='left')
+
+# 업무유형
+time.sleep(1)
+pyautogui.moveTo(3231, 888)
+pyautogui.click(button='left')
+pyautogui.click(button='left')
+pyautogui.moveTo(2976, 404)
+pyautogui.click(button='left')
---
+++ Script/Python/CRMEasy/FindPoint.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+
+# 현재 사용하는 모니터의 해상도 출력
+# print(pyautogui.size())
+
+# 현재 마우스 커서의 위치 출력
+print(pyautogui.position())
+
